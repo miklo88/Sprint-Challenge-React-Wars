dd88e820e0a10c647e7b9e63f3b9250b0f7121b0
Carlos <miklo.concepcion@gmail.com>
2019-08-30 18:34:58 -0500

    first commit cleaning/preparing enviornment for set up. Completing the questions first.

---
+++ sprint-react-wars/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Nothing better than having your bi-weekly sprint test
+          <br></br>
+          :D
+          <br></br> 
+          Happy hacking ya goofballs 
+        </p>
+      
+    </div>
+  );
+}
+
+export default App;
